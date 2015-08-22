84e1bf506733fbcfb7ee8d6405d74084f9416f7f
Anurag Bisht <anuragbisht91@gmail.com>
2015-08-22 13:16:14 +0530

    initial commit

---
+++ RAAGNavigationDrawer/RAAGNavigationDrawer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RAAGNavigationDrawer
+//
+//  Created by Vikram Bisht on 8/22/15.
+//  Copyright (c) 2015 Anurag Bisht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RAAGNavigationDrawer/RAAGNavigationDrawer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RAAGNavigationDrawer
+//
+//  Created by Vikram Bisht on 8/22/15.
+//  Copyright (c) 2015 Anurag Bisht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
